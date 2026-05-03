7c4a4b3479085c1969a4782e3b2cfa7840a83786
Voornaamenachternaam <84537103+Voornaamenachternaam@users.noreply.github.com>
2026-05-03 23:01:55 +0000

    Fix: wrap WP_Error class in class_exists guard
    - WP_User was already wrapped: if (!class_exists('WP_User'))
    - WP_Error was missing the guard, would cause fatal if WordPress loads first
    - PHP fatal: Cannot declare class WP_Error because it already exists

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -266,64 +266,66 @@ if (!function_exists('is_wp_error')) {
     }
 }
 
-class WP_Error
-{
-    public $errors = array();
-    public $error_data = array();
-
-    public function __construct($code = '', $message = '', $data = '')
+if (!class_exists('WP_Error')) {
+    class WP_Error
     {
-        if (empty($code)) {
-            return;
-        }
-        $this->errors[$code] = array($message);
-        if (!empty($data)) {
-            $this->error_data[$code] = $data;
-        }
-    }
+        public $errors = array();
+        public $error_data = array();
 
-    public function get_error_code()
-    {
-        $codes = $this->get_error_codes();
-        if (empty($codes)) {
-            return '';
+        public function __construct($code = '', $message = '', $data = '')
+        {
+            if (empty($code)) {
+                return;
+            }
+            $this->errors[$code] = array($message);
+            if (!empty($data)) {
+                $this->error_data[$code] = $data;
+            }
         }
-        return $codes[0];
-    }
 
-    public function get_error_message($code = '')
-    {
-        if (empty($code)) {
-            $code = $this->get_error_code();
+        public function get_error_code()
+        {
+            $codes = $this->get_error_codes();
+            if (empty($codes)) {
+                return '';
+            }
+            return $codes[0];
         }
-        $messages = $this->get_error_messages($code);
-        if (empty($messages)) {
-            return '';
-        }
-        return $messages[0];
-    }
 
-    public function get_error_codes()
-    {
-        if (empty($this->errors)) {
+        public function get_error_message($code = '')
+        {
+            if (empty($code)) {
+                $code = $this->get_error_code();
+            }
+            $messages = $this->get_error_messages($code);
+            if (empty($messages)) {
+                return '';
+            }
+            return $messages[0];
+        }
+
+        public function get_error_codes()
+        {
+            if (empty($this->errors)) {
+                return array();
+            }
+            return array_keys($this->errors);
+        }
+
+        public function get_error_messages($code = '')
+        {
+            if (empty($code)) {
+                $all_messages = array();
+                foreach ((array) $this->errors as $code => $messages) {
+                    $all_messages = array_merge($all_messages, $messages);
+                }
+                return $all_messages;
+            }
+            if (isset($this->errors[$code])) {
+                return $this->errors[$code];
+            }
             return array();
         }
-        return array_keys($this->errors);
-    }
-
-    public function get_error_messages($code = '')
-    {
-        if (empty($code)) {
-            $all_messages = array();
-            foreach ((array) $this->errors as $code => $messages) {
-                $all_messages = array_merge($all_messages, $messages);
-            }
-            return $all_messages;
-        }
-        if (isset($this->errors[$code])) {
-            return $this->errors[$code];
-        }
-        return array();
     }
 }
 
